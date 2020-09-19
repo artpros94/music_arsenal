106f2138e6f66f4af2fe1e56bb1f538c6f319f16
artpros94 <prostakishyn.artem@icloud.com>
2020-09-19 12:23:12 +0300

    header slider slider-partner

---
+++ app/js/main.js
@@ -0,0 +1,16 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        autoplay: true,
+        // dots: true,
+        arrows: false,
+    });
+
+    $('.partner__inner').slick({
+        autoplay: true,
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        arrows: false,
+    });
+
+});
