0b748cb5e6563e697649d4632f56c8deac2c70c2
artpros94 <prostakishyn.artem@icloud.com>
2020-09-20 20:37:41 +0300

    responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
 
     $('.slider__inner').slick({
-        autoplay: true,
+        // autoplay: true,
         dots: true,
         arrows: false,
     });
@@ -12,14 +12,63 @@ $(function(){
         slidesToScroll: 1,
         prevArrow: '<div class="slick-prev"></div>',
         nextArrow: '<div class="slick-next"></div>',
+        responsive: [
+            {
+                breakpoint: 1030,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,  
+                }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,  
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,  
+                }
+            },
+            {
+                breakpoint: 550,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,  
+                }
+            },
+            {
+                breakpoint: 440,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,  
+                }
+            },
+        ]
     });
 
     $('.stock__items').slick({
-        autoplay: true,
+        // autoplay: true,
         slidesToShow: 2,
         slidesToScroll: 1,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,  
+                }
+            },
+        ] 
     });
 
+    $('.header__menu-btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
 
 });
